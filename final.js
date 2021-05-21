205aa254d444b28132470eb0d628a19e70a14041
The Lienhard <60452107+SupremeEvilOverlord@users.noreply.github.com>
2021-05-21 15:05:25 -0700

    Update final.js

--- final.js
+++ final.js
@@ -1,41 +1,83 @@
 let feels = [{
-  name: "happy",
-  color: "yellow",
+  name: "brings the warmth that only I hold.", //To leave all emotions and have this one behold.
+  color: " ",
+  red: 255,
+  green: 251,
+  blue: 0,
+  image: 'photos/pics_4.png'
+},{
+  name: "To leave all emotions and have this one behold.",
+  color: " ",
   red: 255,
   green: 251,
   blue: 0,
   image: 'photos/pics_4.png'
 }, {
-  name: "sad",
-  color: "blue",
+  name: "With a blockade or even a façade.", //Changing the way you may feel
+color: " ",
   red: 18,
   green: 255,
   blue: 251,
   image: 'photos/pics_1.png'
 }, {
-  name: "melancholey",
-  color:"green",
+  name: "Changing the way you may feel",
+color: " ",
+  red: 18,
+  green: 255,
+  blue: 251,
+  image: 'photos/pics_1.png'
+}, {
+  name: "where you didn't know me not knowing her", //It was all just in a different fantasy
+  color:" ",
+  red: 59,
+  green: 522,
+  blue: 38,
+  image: 'photos/pics_5.png'
+},{
+  name: "It was all just in a different fantasy",
+  color:" ",
   red: 59,
   green: 522,
   blue: 38,
   image: 'photos/pics_5.png'
 }, {
-  name: "surprise",
-  color:"orange",
+  name: "to send a meme instead of words", //you cannot even give me a sight or surprise
+  color:" ",
   red: 255,
   green: 156,
   blue: 43,
   image: 'photos/pics_2.png'
 }, {
-  name: "angry",
-  color:"red",
+  name: "you cannot even give me a sight or surprise",
+  color:" ",
+  red: 255,
+  green: 156,
+  blue: 43,
+  image: 'photos/pics_2.png'
+}, {
+  name: "the doubt and betrayls for this once more", //rage is the way
+  color:" ",
   red: 191,
   green: 0,
   blue: 0,
   image: 'photos/pics_6.png'
 }, {
-  name: "lonely",
-  color:"black",
+  name: "rage is the way",
+  color:" ",
+  red: 191,
+  green: 0,
+  blue: 0,
+  image: 'photos/pics_6.png'
+},{
+  name: "I stood waiting for you to come.", //I stood waiting for you to come.
+  color:" ",
+  red: 168,
+  green: 168,
+  blue: 168,
+  image: 'photos/pics_3.png'
+}, {
+  name: "Knowing that the sun wouldn't rise from the clouds that day",
+  color:" ",
   red: 168,
   green: 168,
   blue: 168,
@@ -52,15 +94,15 @@ let feels = [{
     }
 
     function setup() {
-      createCanvas(600, 600);
-      background(85, 250, 209);
-    /* textSize(32);
+      createCanvas(2000, 1000);
+      background(255);
+     textSize(32);
       textFont('Courier new');
-      textALign(LEFT);
+      //textALign(CENTER);
       frameRate(8);
-      */
 
-      text("click to find your emotion", 50, 50);
+
+      text("The Feeling machine", 50, 50);
 
       //createButton("click to find emotion");
     }
@@ -86,10 +128,15 @@ let feels = [{
        noStroke();
        loadImage(feels[randomIndex].image, img => {
          image(img, 100, 110, 400, 400);
-       });
-       square(random(width), random(height), random(0, 150), random(0, 50));
+         image(img, random(width), random(height), random(width), random(height));
 
-     }
+       });
+       //square(random(width), random(height), random(0, 150), random(0, 50));
+       loadImage(feels[randomIndex].image, img => {
+       image(img, random(width), random(height), random(width), random(height));
+
+     });
+   }
     }
 
     function randomizer() {
@@ -100,7 +147,9 @@ let feels = [{
         feels.splice(randomIndex, 1);
       } else {
         background(85, 250, 209);
-        text("no emotion!", 50, 50);
+        text("How are you feeling now!", 50, 50);
+        text("Has a sense of you cleared a bit now?", 100, 100);
+
       }
     }
 
